455709a928e8f282fa07118b2ac45aad08e8da5f
Richard Ashley <richard@brashley.net>
2015-06-06 20:47:38 -0500

    Added plot 4

--- plot4.R
+++ plot4.R
@@ -24,12 +24,24 @@ setkey(hpc,date_time)
 hpc_set <- hpc[date_time>=ymd_hms("2007-02-01 00:00:00")][date_time<ymd_hms("2007-02-03 00:00:00")]
 
 # open png graphics device
-png(file = "plot3.png", 
+png(file = "plot4.png", 
     bg = "transparent",
     width = 480, 
     height = 480)
 
-# generate plot
+par(mfcol = c(2, 2),
+    cex = 0.8,
+    mar = c(4,5,3,1))
+
+# generate plots
+
+# plot 1
+plot(hpc_set$date_time, hpc_set$Global_active_power, 
+     xlab="",
+     ylab="Global Active Power",
+     type = "l")
+
+# plot 2
 plot(hpc_set$date_time, hpc_set$Sub_metering_1, 
      xlab="",
      ylab="Energy sub meetering",
@@ -39,7 +51,20 @@ lines(hpc_set$date_time,hpc_set$Sub_metering_2, col = "red" ,type = 'l')
 lines(hpc_set$date_time,hpc_set$Sub_metering_3, col = "blue" ,type = 'l')
 legend("topright", 
        legend=colnames(hpc_set)[7:9],
-       lty=c(1,1),
-       col=c("black","red","blue"))           # gives the legend appropriate symbols (lines) and color 
-       
+       lty=c(1,1),                    # gives the legend appropriate symbols (lines) and color 
+       col=c("black","red","blue"),
+       bty = "n")                     # remove legend boarder
+
+# plot 3
+plot(hpc_set$date_time, hpc_set$Voltage, 
+     xlab="datetime",
+     ylab="Voltage",
+     type = "l")
+
+# plot 4
+plot(hpc_set$date_time, hpc_set$Global_reactive_power, 
+     xlab="datetime",
+     ylab="Global_reactive_power",
+     type = "l")
+
 dev.off()
